fb1ed80bc4462e64de23c968f797658cdfcec7cc
IceScout26 <112707262+IceScout26@users.noreply.github.com>
2022-12-18 01:27:34 +0700

    body fixed position

--- jakpajak.com/indexuser/pajak/daftarpajakindividu.php
+++ jakpajak.com/indexuser/pajak/daftarpajakindividu.php
@@ -20,13 +20,13 @@
     <title>Document</title>
 </head>
 
-<body>
+<body class="d-flex flex-column min-vh-100">
     <header>
         <nav class="navbar navbar-dark bg-secondary fixed-top">
             <div class="container-fluid">
                 <a class="navbar-brand text-warning" href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-coin"
-                        viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-coin" viewBox="0 0 16 16">
                         <path
                             d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z" />
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -53,60 +53,81 @@
                             </li>
                             <li class="border-top"></li>
                             <li class="mb-1 text-info">
-                                <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
-                                    data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+                                <button
+                                    class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+                                    data-bs-toggle="collapse" data-bs-target="#dashboard-collapse"
+                                    aria-expanded="false">
                                     Pendataan
                                 </button>
                                 <div class="collapse" id="dashboard-collapse">
                                     <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                        <li><a href="daftarpajakindividu.html" class="link-dark d-inline-flex text-decoration-none rounded">Daftar Pajak
+                                        <li><a href="daftarpajakindividu.html"
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Daftar
+                                                Pajak
                                                 Penghasilan</a>
                                         </li>
-                                        <li><a href="daftarpajakperusahaan.html" class="link-dark d-inline-flex text-decoration-none rounded">Daftar Pajak
+                                        <li><a href="daftarpajakperusahaan.html"
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Daftar
+                                                Pajak
                                                 Perusahaan</a></li>
-                                        <li><a href="daftarpajakkendaraan.html" class="link-dark d-inline-flex text-decoration-none rounded">Daftar Pajak
+                                        <li><a href="daftarpajakkendaraan.html"
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Daftar
+                                                Pajak
                                                 Kendaraan</a>
                                         </li>
-                                        <li><a href="daftarpajakpbb.html" class="link-dark d-inline-flex text-decoration-none rounded">Daftar pajak
-                                            Tanah dan Bangunan</a>
-                                    </li>
+                                        <li><a href="daftarpajakpbb.html"
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Daftar
+                                                pajak
+                                                Tanah dan Bangunan</a>
+                                        </li>
                                     </ul>
                                 </div>
                             </li>
                             <li class="mb-1 text-info">
-                                <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+                                <button
+                                    class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
                                     data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
                                     Informasi
                                 </button>
                                 <div class="collapse" id="orders-collapse">
                                     <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                        <li><a href="informasi/informasipajakindividu.html" class="link-dark d-inline-flex text-decoration-none rounded">Pajak Penghasilan</a>
+                                        <li><a href="informasi/informasipajakindividu.html"
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Pajak
+                                                Penghasilan</a>
                                         </li>
                                         <li><a href="informasi/informasipajakperusahaan.html"
-                                                class="link-dark d-inline-flex text-decoration-none rounded">Pajak Perusahaan</a>
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Pajak
+                                                Perusahaan</a>
                                         </li>
-                                        <li><a href="informasi/informasipajakkendaraan.html" class="link-dark d-inline-flex text-decoration-none rounded">Pajak Kendaraan</a>
+                                        <li><a href="informasi/informasipajakkendaraan.html"
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Pajak
+                                                Kendaraan</a>
                                         </li>
                                         <li><a href="informasi/informasipajakpbb.html"
-                                                class="link-dark d-inline-flex text-decoration-none rounded">Pajak Tanah dan Bangunan</a>
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Pajak Tanah
+                                                dan Bangunan</a>
                                         </li>
                                     </ul>
                                 </div>
                             </li>
                             <li class="border-top my-3"></li>
                             <li class="mb-1 text-info">
-                                <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+                                <button
+                                    class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
                                     data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
                                     Akun
                                 </button>
                                 <div class="collapse" id="account-collapse">
                                     <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                        <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Profile</a>
+                                        <li><a href="#"
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Profile</a>
                                         </li>
                                         <li><a href="#"
-                                                class="link-dark d-inline-flex text-decoration-none rounded">Profile Settings</a>
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Profile
+                                                Settings</a>
                                         </li>
-                                        <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Sign
+                                        <li><a href="#"
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Sign
                                                 out</a>
                                         </li>
                                     </ul>
@@ -145,41 +166,46 @@
                         <td><?php echo $user_data['persenpjk'] ?>%</td>
                         <td>IDR.<?php echo $user_data['pajak'] ?></td>
                         <td>
-                            <a class='btn btn-warning' href="#" data-bs-toggle="modal" data-bs-target="#updata<?php echo $no ?>">Edit</a>
+                            <a class='btn btn-warning' href="#" data-bs-toggle="modal"
+                                data-bs-target="#updata<?php echo $no ?>">Edit</a>
                             <!-- <a class='btn btn-danger' href="deletedata/deletepajakindividu.php?id=<?php echo $user_data['id']?>">Delete</a> -->
                             <a onclick=delete_data(<?=$user_data['id']?>) class='btn btn-danger'>Delete</a>
                         </td>
                     </tr>
                 </tbody>
-                    
-                <!--FORM UPDATE-->
-                <div class="modal fade" id="updata<?= $no ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Update Data</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
 
-                    <!--FORM-->
-                    <form action="./editdata/editpajakindividu.php" method="POST">
-                        <input type="hidden" name="id" value="<?= $user_data['id']?>">
-                        <div class="modal-body">
-                            <div class="mb-3">
-                                <label class="form-label">Nama</label>
-                                <input type="text" class="form-control" name="nama" value="<?=$user_data['nama']?>">
+                <!--FORM UPDATE-->
+                <div class="modal fade" id="updata<?= $no ?>" data-bs-backdrop="static" data-bs-keyboard="false"
+                    tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Update Data</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
                             </div>
-                            <div class="mb-3">
-                                <label class="form-label">Gaji</label>
-                                <input type="text" class="form-control" name="gaji" value="<?=$user_data['gaji']?>">
-                            </div>
+
+                            <!--FORM-->
+                            <form action="./editdata/editpajakindividu.php" method="POST">
+                                <input type="hidden" name="id" value="<?= $user_data['id']?>">
+                                <div class="modal-body">
+                                    <div class="mb-3">
+                                        <label class="form-label">Nama</label>
+                                        <input type="text" class="form-control" name="nama"
+                                            value="<?=$user_data['nama']?>">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label">Gaji</label>
+                                        <input type="text" class="form-control" name="gaji"
+                                            value="<?=$user_data['gaji']?>">
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-success" name="update">Update</button>
+                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </form>
                         </div>
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-success" name="update">Update</button>
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                        </div>
-                    </form>
-                    </div>
                     </div>
                 </div>
                 <!--FORM AKHIR UPDATE-->
@@ -187,33 +213,34 @@
             </table>
 
             <!--FORM AWAL DATA-->
-            <div class="modal fade" id="adddata" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal fade" id="adddata" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Data</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    
-                    <!--FORM-->
-                    <form action="inputdata/inputpajakindividu.php" method="POST">
-                    <div class="modal-body">
-                        <div class="mb-3">
-                            <label class="form-label">Nama</label>
-                            <input type="text" class="form-control" name="nama">
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Gaji</label>
-                            <input type="text" class="form-control" name="gaji">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Data</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
 
+                        <!--FORM-->
+                        <form action="inputdata/inputpajakindividu.php" method="POST">
+                            <div class="modal-body">
+                                <div class="mb-3">
+                                    <label class="form-label">Nama</label>
+                                    <input type="text" class="form-control" name="nama">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Gaji</label>
+                                    <input type="text" class="form-control" name="gaji">
+                                </div>
 
-                    </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-success" name="inputpajakindividu">Add</button>
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                    </div>
-                    </form>
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-success" name="inputpajakindividu">Add</button>
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </form>
 
                     </div>
                 </div>
@@ -221,7 +248,8 @@
             <!--FORM AKHIR DATA-->
 
             <div class="d-grid gap-2 col-6 mx-auto">
-                <a class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#adddata">Tambah Data</a>
+                <a class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#adddata">Tambah
+                    Data</a>
                 <a class="btn btn-warning" type="button" href="../index2.html">Back</a>
             </div>
             <!-- <div class="d-grid gap-2 col-6 mx-auto">
@@ -230,17 +258,14 @@
             </div> -->
         </nav>
     </main>
-    
+
     <script type="text/javascript">
-    function delete_data(uid)
-    {
-        if (confirm('Yakin ingin hapus?'))
-        {
-            window.location.href = 'deletedata/deletepajakindividu.php?id='+uid;
+    function delete_data(uid) {
+        if (confirm('Yakin ingin hapus?')) {
+            window.location.href = 'deletedata/deletepajakindividu.php?id=' + uid;
         }
     }
-    
-  </script>
+    </script>
 </body>
 
 </html>
