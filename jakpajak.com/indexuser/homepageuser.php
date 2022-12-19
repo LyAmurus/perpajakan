6658a76ff506a2ab4acd817f5608344da732c7fd
IceScout26 <112707262+IceScout26@users.noreply.github.com>
2022-12-19 23:33:40 +0700

    update link kostan

--- jakpajak.com/indexuser/homepageuser.php
+++ jakpajak.com/indexuser/homepageuser.php
@@ -300,7 +300,7 @@
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
       © 2022 Copyright:
-      <a class="text-white" href="#">KostanPakSuprapto.Com</a>
+      <a class="text-white" href="https://goo.gl/maps/N1NRsvC4zvSrPQxx8">KostanPakSuprapto.Com</a>
     </div>
     <!-- Copyright -->
   </footer>
