9ce79d23f77d6cc39f2faa06f7b671a7cf384820
IceScout26 <112707262+IceScout26@users.noreply.github.com>
2022-12-20 00:32:47 +0700

    fixed title

--- jakpajak.com/indexuser/pajak/daftarpajakkendaraan.php
+++ jakpajak.com/indexuser/pajak/daftarpajakkendaraan.php
@@ -16,13 +16,8 @@ $result = mysqli_query($conn, "SELECT * FROM data_pbb");
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-<<<<<<< HEAD
-        </script>
-    <title>Document</title>
-=======
     </script>
     <title>Pajak PBB</title>
->>>>>>> e56ca75853f4406ae118680359b1dc8bf40093b7
 </head>
 
 
