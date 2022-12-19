68108e7a77153d5bb317cc3d5871ac96ca3f8f71
Raka Adi Prasetya <115936880+raanui21@users.noreply.github.com>
2022-12-19 20:23:27 +0700

    crotddddddddddddddddddddd

---
+++ jakpajak.com/indexuser/homepageuser.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-dark bg-secondary fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand text-warning" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-coin"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z" />
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
+                    </svg>
+                    <span class="fs-5 fw-semibold">JakPajak.com</span>
+                </a>
+                <button-toggle class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button-toggle>
+                <div class="offcanvas offcanvas-end text-bg-secondary" tabindex="-1" id="offcanvasDarkNavbar"
+                    aria-labelledby="offcanvasDarkNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                            aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="border-top"></li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index2.html">Home</a>
+                            </li>
+                            <li class="border-top"></li>
+                            <li class="mb-1 text-info">
+                                <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+                                    data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+                                    Pendataan
+                                </button>
+                                <div class="collapse" id="dashboard-collapse">
+                                    <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                        <li><a href="assets/daftarpajakindividu.html" class="link-dark d-inline-flex text-decoration-none rounded">Daftar Pajak
+                                                Penghasilan</a>
+                                        </li>
+                                        <li><a href="assets/daftarpajakperusahaan.html" class="link-dark d-inline-flex text-decoration-none rounded">Daftar Pajak
+                                                Perusahaan</a></li>
+                                        <li><a href="assets/daftarpajakkendaraan.html" class="link-dark d-inline-flex text-decoration-none rounded">Daftar Pajak
+                                                Kendaraan</a>
+                                        </li>
+                                        <li><a href="assets/daftarpajakpbb.html" class="link-dark d-inline-flex text-decoration-none rounded">Daftar pajak
+                                            Tanah dan Bangunan</a>
+                                    </li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="mb-1 text-info">
+                                <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+                                    data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+                                    Informasi
+                                </button>
+                                <div class="collapse" id="orders-collapse">
+                                    <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                        <li><a href="assets/informasi/informasipajakindividu.html" class="link-dark d-inline-flex text-decoration-none rounded">Pajak Penghasilan</a>
+                                        </li>
+                                        <li><a href="assets/informasi/informasipajakperusahaan.html"
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Pajak Perusahaan</a>
+                                        </li>
+                                        <li><a href="assets/informasi/informasipajakkendaraan.html" class="link-dark d-inline-flex text-decoration-none rounded">Pajak Kendaraan</a>
+                                        </li>
+                                        <li><a href="assets/informasi/informasipajakpbb.html"
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Pajak Tanah dan Bangunan</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="border-top my-3"></li>
+                            <li class="mb-1 text-info">
+                                <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed"
+                                    data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+                                    Akun
+                                </button>
+                                <div class="collapse" id="account-collapse">
+                                    <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                        <li><a href="assets/profile/profil.html" class="link-dark d-inline-flex text-decoration-none rounded">Profile</a>
+                                        </li>
+                                        <li><a href="assets/profile/updateprofile.php"
+                                                class="link-dark d-inline-flex text-decoration-none rounded">Profile Settings</a>
+                                        </li>
+                                        <li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Sign
+                                                out</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    
+    <main>
+        <div class="header-3-3 container-xxl mx-auto p-5 position-relative" style="font-family: 'Poppins', sans-serif">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="modal" data-bs-target="#targetModal-item">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="modal-item modal fade" id="targetModal-item" tabindex="-1" role="dialog"
+          aria-labelledby="targetModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content bg-white border-0">
+              <div class="modal-header border-0" style="padding: 2rem; padding-bottom: 0">
+                <a class="modal-title" id="targetModalLabel">
+                  <img style="margin-top: 0.5rem"
+                    src="http://api.elements.buildwithangga.com/storage/files/2/assets/Header/Header3/Header-3-6.png"
+                    alt="" />
+                </a>
+                <button type="button" class="close btn-close text-white" data-bs-dismiss="modal"
+                  aria-label="Close"></button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo">
+          <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+            <li class="nav-item my-auto">
+              <a class="nav-link" data-bs-toggle="collapse" href="#collapse" role="button" aria-expanded="false"
+                aria-controls="collapse">
+                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M5.85 1.69346C3.5304 1.69346 1.65 3.57386 1.65 5.89346C1.65 8.21305 3.5304 10.0935 5.85 10.0935C8.1696 10.0935 10.05 8.21305 10.05 5.89346C10.05 3.57386 8.1696 1.69346 5.85 1.69346ZM0.25 5.89346C0.25 2.80066 2.75721 0.293457 5.85 0.293457C8.94279 0.293457 11.45 2.80066 11.45 5.89346C11.45 8.98625 8.94279 11.4935 5.85 11.4935C2.75721 11.4935 0.25 8.98625 0.25 5.89346Z"
+                    fill="#8B9CAF" />
+                  <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M8.85503 8.89848C9.12839 8.62512 9.57161 8.62512 9.84497 8.89848L14.045 13.0985C14.3183 13.3718 14.3183 13.8151 14.045 14.0884C13.7716 14.3618 13.3284 14.3618 13.055 14.0884L8.85503 9.88843C8.58166 9.61506 8.58166 9.17185 8.85503 8.89848Z"
+                    fill="#8B9CAF" />
+                </svg>
+              </a>
+              <form class="collapse position-absolute form center-search border-0" id="collapse">
+                <div class="d-flex">
+                  <input type="text" class="rounded-full border-0 focus:outline-none" placeholder="Search" />
+                  <button class="btn" type="button">
+                    <svg style="width: 20px; height: 20px" data-bs-toggle="collapse" href="#collapse" role="button"
+                      aria-expanded="false" aria-controls="collapse" fill="none" stroke="#273B56" viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
+                      </path>
+                    </svg>
+                  </button>
+                </div>
+              </form>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div class="hr">
+        <hr style="
+							border-color: #F4F4F4;
+							background-color: #F4F4F4;
+							opacity: 1;
+							margin: 0 !important;
+						" />
+      </div>
+
+      <div>
+        <div class="mx-auto d-flex flex-lg-row flex-column hero">
+          <!-- Left Column -->
+          <div
+            class="left-column flex-lg-grow-1 d-flex flex-column align-items-lg-start text-lg-start align-items-center text-center">
+            <h1 class="title-text-big">
+              Apakah<br class="d-lg-block d-none" />
+              <span style="color: #4E91F9">Bayar Pajak</span> Itu<br class="d-lg-block d-none" />
+              Wajib?
+            </h1>
+            <p class="text-caption">
+              Pajak adalah iuran atau pungutan wajib yang harus dibayar oleh rakyat (wajib pajak) kepada negara berdasarkan undang-undang, 
+              dimana uang pajak tersebut akan digunakan untuk kepentingan pemerintah dan kesejahteraan masyarakat umum.<br class="d-sm-block d-none" />
+            </p>
+            <div class="d-flex flex-sm-row flex-column align-items-center mx-auto mx-lg-0 justify-content-center gap-3">
+              <button class="btn btn-get text-white d-inline-flex">
+                Bayar Pajak
+              </button>
+            </div>
+          </div>
+
+          <!-- Right Column -->
+          <div class="right-column d-flex justify-content-center justify-content-lg-start text-center pe-0">
+            <img class="position-absolute d-lg-block d-none hero-right"
+              src="http://api.elements.buildwithangga.com/storage/files/2/assets/Header/Header3/Header-3-2.png"
+              alt="" />
+            <div class="d-flex align-items-end card-outer">
+              <div class="mx-auto d-flex flex-wrap align-items-center">
+                <div class="card border-0 position-relative d-flex flex-column">
+                  <div class="d-flex align-items-center" style="margin-bottom: 1.25rem">
+                    <div>
+                      <img style="margin-right: 1rem"
+                        src="http://api.elements.buildwithangga.com/storage/files/2/assets/Header/Header3/Header-3-3.png"
+                        alt="" />
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </main>
+
+    <footer>
+        <!--jelas buat footer-->
+    </footer>
+</body>
+
+</html>
---
+++ jakpajak.com/indexuser/pajak/profile/updateprofile.php
@@ -0,0 +1,202 @@
+<?php
+
+include 'connection/konek.php';
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+$fname = isset ($_POST['fname']) ? $_POST['fname'] : '';
+$lname = isset ($_POST['lname']) ? $_POST['lname'] : '';
+$nik = isset ($_POST['nik']) ? $_POST['nik'] : '';
+$adress = isset ($_POST['adress']) ? $_POST['adress'] : '';
+$email = isset ($_POST['email']) ? $_POST['email'] : '';
+
+
+
+if (isset($_POST['submit'])) {
+    $usersaatini = $_SESSION['username'];
+    $sql = "UPDATE data_user SET fname='$fname', lname='$lname', nik='$nik', adress='$adress', email='$email' WHERE username='$usersaatini'";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        echo "<script>alert('Data berhasil diupdate!')</script>";
+        header("Location: ../../homepageuser.php");
+    } else {
+        echo "<script>alert('Data gagal diupdate!')</script>";
+    }
+}
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User</title>
+    <link rel="stylesheet" href="assets/settinguser/style.css">
+    <link rel="stylesheet" href="assets/settinguser/responsive.css">
+    <link rel="shortcut icon" href="assets/settinguser/users.svg" type="image/x-icon">
+
+</head>
+
+<body>
+    <header>
+        <div class="big-header">
+            <div class="left">
+                <div class="logo">
+                    <h1>JakPajak</h1>
+                </div>
+                <div class="menu-Items">
+                    <ul class="main-Ul dul-color">
+                        <li>Home</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="right">
+                <div class="user-avtar">
+                    <div class="imgg">
+                        <img src="https://i.ibb.co/FKXL7gR/Whats-App-Image-2022-12-17-at-12-03-41.jpg" alt="">
+                    </div>
+                </div>
+                <div class="button">
+                    <button class="logout-btn">Logout</button>
+                </div>
+                <img src="https://i.ibb.co/FKXL7gR/Whats-App-Image-2022-12-17-at-12-03-41.jpg" class="menu-icon" alt="">
+            </div>
+        </div>
+    </header>
+
+
+    <section>
+        <div class="acc-menu-open-dots">
+            <img src="https://i.ibb.co/FKXL7gR/Whats-App-Image-2022-12-17-at-12-03-41.jpg" alt="" id="accMenuThreeDots">
+        </div>
+    </section>
+
+    <section>
+        <div class="account-Menu">
+            <div class="account-Menu-sect-items">
+                <button>Account</button>
+                <button>Socail Profiles</button>
+            </div>
+        </div>
+    </section>
+    <section class="second-section">
+        <div class="main-account-page-content">
+            <div class="sec-right">
+                <div class="sect-heading">
+                    <h1>Settings Account</h1>
+                </div>
+                <div class="sect-items">
+                    <p id="accountItem">Account</p>
+                    <p id="logOutItem">LogOut</p>
+                </div>
+            </div>
+
+
+
+<!-- Account Section -->
+            <div class="sect-left" id="accountSection">
+                <div class="dect-left-heading-1">
+                    <h1>Account</h1>
+                </div>
+                <div class="sect-left-avatar-2">
+                    <div class="sect-left-avatar-img">
+                        <img src="https://i.ibb.co/FKXL7gR/Whats-App-Image-2022-12-17-at-12-03-41.jpg" alt="">
+                    </div>
+                    <div class="name-bio">
+                        <p class="user-name" id="userNameAccount">
+                            User
+                        </p>
+                        <p class="bio" id="UserBioAccount">
+                            .
+                        </p>
+                    </div>
+                </div>
+                <div class="btns">
+                    <button>Upload</button>
+                </div>
+                <hr size="1px" style="margin: 30px 0px 10px 0px;">
+                
+
+                <div class="account-edit-form-5">
+                    <form method="post" class="acc-edit-frm">
+                        <?php 
+                        $usersaatini = $_SESSION['username'];
+                        $sql = "SELECT * FROM data_user WHERE username='$usersaatini'";
+                        $result = mysqli_query($conn, $sql);
+                        if($result){
+                            if(mysqli_num_rows($result)>0){
+                                while($row = mysqli_fetch_array($result)){
+                                    $id = $row['id'];
+                                    $fname = $row['fname'];
+                                    $lname = $row['lname'];
+                                    $nik = $row['nik'];
+                                    $adress = $row['adress'];
+                                    $email = $row['email'];
+                                }
+                            }
+                        }
+                        ?>
+                        <div class="form-1" >
+                            <div class="inner-form-1">
+                            <input type="hidden" name="id" value="<?php echo $id ?>"/>
+                                <p>First Name</p>
+                                <input type="text" name="fname" value="<?php echo $fname; ?>">
+                            </div>
+                            <div class="inner-form-2">
+                                <p>Last Name</p>
+                                <input type="text" name="lname" value="<?php echo $lname; ?>">
+                            </div>
+                        </div>
+                        <div class="form-2">
+                            <div class="inner-form-2-1">
+                                <p>NIK</p>
+                                <input type="Nik" name="nik" value="<?php echo $nik; ?>">
+                            </div>
+                            <div class="inner-form-2-2">
+                                <p>Tanggal Lahir</p>
+                                <input type="text" id="TanggalLahir"value="<?php ?>">
+                                </div>
+                            <div class="form-3">
+                            <div class="inner-form-3-1">
+                                <p>Email</p>
+                                <input type="email" name="email"value="<?php echo $email; ?>">
+                            </div>
+                                <div class="inner-form-3-1">
+                                    <p>Address</p>
+                                    <input type="Address" name="adress"value="<?php echo $adress; ?>">
+                                </div>
+                            </div>
+                            <input type="submit" name="submit"class="save-btn" value="submit">
+                            <a href=" ../homepageuser.php" class="btn btn-defauly">Cancel</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="main-account-page-content-mobile" id="mobileAccMenuSection">
+            <div class="sec-right-mobile">
+                <div class="sect-heading-mobile">
+                    <h1>Settings</h1>
+                    <img src="https://i.ibb.co/FKXL7gR/Whats-App-Image-2022-12-17-at-12-03-41.jpg" alt="" id="accMenuCross">
+                </div>
+                <div class="sect-items-mobile">
+                    <p id="accountItemMobile">Account</p>
+                    <p id="logOutItemMobile">LogOut</p>
+                </div>
+            </div>
+    </section>
+
+
+</body>
+
+</html>
+
