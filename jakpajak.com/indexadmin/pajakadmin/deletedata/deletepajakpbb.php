9d2db71c29dc16bb7f00b8c96ff202904e33b775
Mori <110575212+elysiamori@users.noreply.github.com>
2022-12-19 20:59:38 +0700

    add to php and database

---
+++ jakpajak.com/indexadmin/pajakadmin/deletedata/deletepajakpbb.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once "../connectdata/conn.php";
+
+if( isset($_GET['id']) ){
+
+    // ambil id dari query
+    $id = $_GET['id'];
+
+    // buat query hapus
+    $data = "DELETE FROM data_pbb WHERE id=$id";
+    $query = mysqli_query($conn, $data);
+
+    if($query){
+        header('Location: ../daftarpajakpbb.php');
+    } 
+    
+    else {
+        die("Gagal delete data");
+    }
+}
+?>
